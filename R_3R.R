3a7216b1fa39c4069721bfc7d723f39f678d1725
glencalel <glencalel95@gmail.com>
2024-10-25 22:46:10 -0600

    Update R_3R.R

--- R_3R.R
+++ R_3R.R
@@ -3,6 +3,8 @@ library(arules)
 # Cargar todos los datos contenidos en el csv a la variable data.
 data <- read.csv('C:\\MIGRACION_BDP.csv', sep=",")
 
+data <- read.csv('MIGRACION_BDP.csv', sep=",")
+
 data <- discretizeDF(data)
 
 clean_data <- data[, !(names(data) %in% c("NUM_VIVIENDA"))]
@@ -10,7 +12,7 @@ clean_data <- data[, !(names(data) %in% c("NUM_VIVIENDA"))]
 clean_data <- clean_data[, !(names(clean_data) %in% c("ZONA"))]
   
 
-fpgrowth_rules <- fim4r(clean_data, method="fpgrowth", target="rules", supp = .2, conf = .5)
+fpgrowth_rules <- apriori(data, parameter = list(support=0.2, confidence=0.5))
 
 
 inspect(fpgrowth_rules[100:200])
